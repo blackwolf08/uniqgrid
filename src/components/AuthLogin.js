897fb2571d08448df58111f44ac62b22c8a18d94
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-26 00:52:27 +0530

    Routes and Components done

---
+++ src/components/AuthLogin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AuthLogin extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import withAuth from "../hocs/withAuth";
+import { connect } from "react-redux";
+import { authUser } from "../actions/auth";
+import AuthLogin from "./AuthLogin";
+import Dashboard from "./Dashboard";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class Login extends Component {
+  render() {
+    const { authUser } = this.props;
+    return (
+      <Switch>
+        <Route
+          path="/login"
+          render={props => <AuthLogin signin onAuth={authUser} {...props} />}
+        />
+        <Route 
+          path="/dashboard"
+          component={withAuth(Dashboard)}
+        />
+      </Switch>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { authUser }
+  )(Login)
+);
