799bfe16c2143fdcf9419e779b6fd459115e7a4e
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-27 16:38:58 +0530

    Update MyProfile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
+import axios from "axios";
 
 export default class MyProfile extends Component {
+  componentDidMount() {
+    const jwt = localStorage.jwtToken;
+    console.log(jwt);
+    const URL = "http://portal.uniqgridcloud.com:8080/api/customers";
+
+    axios.get(URL).then(e => {
+      console.log(e);
+    });
+  }
+
   render() {
     return (
       <div>
